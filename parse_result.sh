36e65df651ff1f5adcd8916ad53358744c3b15a6
Daria Chen <smartexist@gmail.com>
2017-08-02 18:14:15 +0800

    feat(travis): add build information.

--- parse_result.sh
+++ parse_result.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# List the upstream build information
+echo "This test is triggered by mcs-lite-app Build#${UPSTREAM_TRAVIS_BUILD_NUMBER} with Commit ID ${UPSTREAM_TRAVIS_COMMIT}"
+
+# List the report URL
+for report in ./report/*
+do
+	report=`basename $report`
+	echo "Please visit report on ${REPORT_URL}$report"
+done
+
+# Check Jmeter test result
 if grep 's="false"' ./*.jtl; then
 	echo found erros
 	exit 1

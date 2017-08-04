f3484f76fbab923664690ac77aff35b6500763f8
Daria Chen <smartexist@gmail.com>
2017-08-04 17:10:12 +0800

    Feature/auto build (#2)
    Disable admin related api test.
    Use some environment variable in S3 setting

--- parse_result.sh
+++ parse_result.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# List the upstream build information
+echo "====================================================================================================================="
+echo "This test is triggered by mcs-lite-app Build#${UPSTREAM_TRAVIS_BUILD_NUMBER} with Commit ID ${UPSTREAM_TRAVIS_COMMIT}"
+
+# List the report URL
+for report in ./report/*
+do
+	report=`basename $report`
+	echo "Please visit report on https://s3-${S3_BUCKET_REGION}.amazonaws.com/${S3_BUCKET_NAME}/$report"
+	echo "====================================================================================================================="
+done
+
+# Check Jmeter test result
 if grep 's="false"' ./*.jtl; then
 	echo found erros
 	exit 1
